c75dfef09067601b96693428a3128070fb208f69
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-22 18:37:10 -0300

    Contagem de movimentos das peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -108,7 +108,7 @@ public class UI {
 	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		System.out.println("PEÇAS CAPTURADAS:");
+		System.out.println("CAPTURADAS:");
 		System.out.print("BRANCAS: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
